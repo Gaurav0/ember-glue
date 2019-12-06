bf55f32756aad8016142d08f15feb265ceeb8259
Gaurav Munjal <Gaurav0@aol.com>
2019-12-06 01:34:08 -0500

    Change test file to typescript

--- packages/@ember-glue/button/tests/integration/components/glue-button-test.ts
+++ packages/@ember-glue/button/tests/integration/components/glue-button-test.ts
@@ -1,19 +1,23 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, settled } from '@ember/test-helpers';
-import { hbs }from 'ember-cli-htmlbars';
+import { render, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import 'qunit-dom';
+import { TestContext } from 'ember-test-helpers';
+
+type Context = TestContext & { handleClick: () => void };
 
 module('Integration | Component | glue-button', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it works with label', async function(assert) {
+  test('it works with label', async function(this: Context, assert) {
     let clickCalled = 0;
     this.handleClick = () => {
       clickCalled++;
     };
 
     await render(hbs`<GlueButton title="My Title" @label="My Button" @handleClick={{action this.handleClick}} />`);
-    await settled();
+
     await click('.glue-button');
 
     assert.dom('.glue-button').containsText('My Button');
