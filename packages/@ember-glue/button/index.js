4ad1c8b3571410c2e636ad6f778dbc39ed2424f9
Gaurav Munjal <Gaurav0@aol.com>
2019-12-06 05:06:53 -0500

    base styles for button

--- packages/@ember-glue/button/index.js
+++ packages/@ember-glue/button/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/styles/base.css');
+  },
 };
