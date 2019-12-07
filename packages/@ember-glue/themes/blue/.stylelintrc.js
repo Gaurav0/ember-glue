9ead178ceca073aa4abb1c1aadffb2877b1a4df3
Gaurav Munjal <Gaurav0@aol.com>
2019-12-07 16:11:30 -0500

    style lint

---
+++ packages/@ember-glue/themes/blue/.stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "stylelint-config-recommended",
+  "rules": {
+    "no-empty-source": null,
+    "function-calc-no-unspaced-operator": null, // trips up on var(--)
+    "function-calc-no-invalid": null // trips up on var(--)
+  }
+}
