6d2953073bdfecadcbd5df0297e1b7120d5b1219
Gaurav Munjal <Gaurav0@aol.com>
2019-12-16 08:58:41 -0500

    make blue theme work with menu

--- packages/@ember-glue/menu/index.js
+++ packages/@ember-glue/menu/index.js
@@ -7,5 +7,12 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     this.import('vendor/styles/menu.css', { prepend: true });
+  },
+
+  contentFor(type, config) {
+    let emberBasicDropdown = this.addons.find(
+      addon => addon.name === 'ember-basic-dropdown'
+    );
+    return emberBasicDropdown.contentFor(type, config);
   }
 };
