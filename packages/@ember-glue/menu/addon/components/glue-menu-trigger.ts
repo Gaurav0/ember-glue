ee1e9cb18065e7ae84c981bff7ff2696d9faae75
Gaurav Munjal <Gaurav0@aol.com>
2019-12-15 19:21:39 -0500

    fix trigger test

--- packages/@ember-glue/menu/addon/components/glue-menu-trigger.ts
+++ packages/@ember-glue/menu/addon/components/glue-menu-trigger.ts
@@ -1,15 +1,9 @@
 import Component from '@glimmer/component';
 
-interface Args {
-  triggerComponent: any;
-}
+interface Args {}
 
 export default class GlueMenuTrigger extends Component<Args> {
   constructor(owner: unknown, args: Args) {
     super(owner, args);
-  }
-
-  get triggerComponent() {
-    return this.args.triggerComponent;
   }
 }
--- packages/@ember-glue/menu/tests/integration/components/glue-menu-trigger-test.ts
+++ packages/@ember-glue/menu/tests/integration/components/glue-menu-trigger-test.ts
@@ -3,17 +3,35 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { TestContext } from 'ember-test-helpers';
+import { Dropdown } from 'ember-basic-dropdown/components/basic-dropdown';
+import { guidFor } from '@ember/object/internals';
 
-type Context = TestContext & { triggerComponent: any };
+type Context = TestContext & {
+  triggerComponent: any;
+  dropdown: Dropdown;
+};
 
 module('Integration | Component | glue-menu-trigger', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it works inline with label', async function(this: Context, assert) {
     this.triggerComponent = 'basic-dropdown-trigger';
+    this.dropdown = {
+      uniqueId: guidFor(this),
+      isOpen: false,
+      disabled: false,
+      actions: {
+        toggle() {}
+      },
+    };
 
-    await render(hbs`<GlueMenuTrigger title="My Title" @label="Some Text" @triggerComponent={{component this.triggerComponent}} />`);
-
+    await render(hbs`
+      <GlueMenuTrigger
+        title="My Title"
+        @label="Some Text"
+        @triggerComponent={{component this.triggerComponent dropdown=this.dropdown}}
+      />
+    `);
     await click('.glue-menu--trigger');
 
     assert.dom('.glue-menu--trigger').containsText('Some Text');
@@ -22,8 +40,23 @@ module('Integration | Component | glue-menu-trigger', function(hooks) {
 
   test('it works as a block element', async function(this: Context, assert) {
     this.triggerComponent = 'basic-dropdown-trigger';
+    this.dropdown = {
+      uniqueId: guidFor(this),
+      isOpen: false,
+      disabled: false,
+      actions: {
+        toggle() {}
+      },
+    };
 
-    await render(hbs`<GlueMenuTrigger title="My Title" @triggerComponent={{component this.triggerComponent}}>Some Text</GlueMenuTrigger>`);
+    await render(hbs`
+      <GlueMenuTrigger
+        title="My Title"
+        @triggerComponent={{component this.triggerComponent dropdown=this.dropdown}}
+      >
+        Some Text
+      </GlueMenuTrigger>
+    `);
 
     await click('.glue-menu--trigger');
 
