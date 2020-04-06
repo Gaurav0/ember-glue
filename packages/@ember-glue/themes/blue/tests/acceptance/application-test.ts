d1704ff5089bbcd8bf2d2f1e0f99f49c9066e750
Gaurav Munjal <Gaurav0@aol.com>
2020-04-06 15:07:22 -0400

    add acceptance test for theme

---
+++ packages/@ember-glue/themes/blue/tests/acceptance/application-test.ts
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | application', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('button.glue-button').exists();
+    assert.dom('label.glue-toggle').exists();
+    assert.dom('input.glue-toggle--checkbox').exists();
+    assert.dom('span.glue-toggle--switch').exists();
+    assert.dom('div.glue-menu').exists();
+    assert.dom('button.glue-menu--trigger').exists();
+
+    await click('button.glue-menu--trigger');
+
+    assert.dom('button.glue-menu--item').exists({ count: 4 });
+  });
+});
